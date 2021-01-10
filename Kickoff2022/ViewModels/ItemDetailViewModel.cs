ba413623559b7a024e55a92a1ad1c781bbc6e600
Hoppenthaler, Tobias (external) <tobias.hoppenthaler.ext@miele.com>
2021-01-10 19:25:51 +0100

    First commit

---
+++ Kickoff2022/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Kickoff2022.Models;
+
+namespace Kickoff2022.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
