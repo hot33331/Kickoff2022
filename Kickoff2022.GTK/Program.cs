4d8191fd67068fc24c9e0ab84c3684300f1cf23e
Hoppenthaler, Tobias (external) <tobias.hoppenthaler.ext@miele.com>
2021-01-10 19:43:54 +0100

    Added GTK

---
+++ Kickoff2022.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+
+namespace Kickoff2022.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Kickoff2021");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
