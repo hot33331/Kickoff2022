136f6e71acb934477e3c2ac60ef8702dfdb18ba9
Tobias Hoppenthaler <tobias.hoppenthaler@gmail.com>
2021-01-11 21:58:10 +0100

    added wpf

---
+++ Kickoff2022.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace Kickoff2022.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new Kickoff2022.App());
+        }
+    }
+}
